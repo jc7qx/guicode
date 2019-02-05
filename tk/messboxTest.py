3073a9baa429904ed57eeddbb4bfdf7d5eec2232
Jainshone Chung <jc7qxccit@gmail.com>
2019-02-05 20:45:56 +0800

    message box

---
+++ tk/messboxTest.py
@@ -0,0 +1,14 @@
+# 訊息框
+
+import tkinter
+from tkinter import messagebox
+
+top = tkinter.Tk()
+
+def hello():
+   messagebox.showinfo("Say Hello", "Hello World")
+
+B1 = tkinter.Button(top, text = "Say Hello", command = hello)
+B1.pack()
+
+top.mainloop()
